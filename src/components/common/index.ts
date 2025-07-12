98d4026f114bbc8661d17d51c3f1e648f4216cc0
KamoteNga <kamoteka143din@gmail.com>
2025-07-12 09:50:54 +0800

    Clean up and prepare for flat folder restructuring

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,4 +1,2 @@
-export { default as Button } from './Button';
-export { default as PrimaryButton } from './PrimaryButton';
 export { default as ScreenHeader } from './ScreenHeader';
 export { default as ConfirmActionModal } from './ConfirmActionModal';
--- src/types/index.ts
+++ src/types/index.ts
@@ -3,10 +3,3 @@ export * from "./match.types";
 export * from "./player.types";
 export * from "./navigation.types";
 
-// export interface Player {
-//   id: string;
-//   name: string;
-//   seed: number;
-//   losses: number;
-//   isEliminated?: boolean;
-// }
